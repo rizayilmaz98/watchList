78df5a1f8efee4a1d13d543fe46afb486c08b6fd
rizayilmaz98 <riza88665@gmail.com>
2023-06-19 23:03:36 +0300

    Update Project

--- src/components/WatchListMovie.js
+++ src/components/WatchListMovie.js
@@ -5,23 +5,26 @@ import MovieCard from "./MovieCard";
 function WatchListMovie() {
   const favoriteMovieData = useSelector((state) => state.movies.favoriteList);
   return (
-    <div className="container-fluid  bg-singleMovie pb-5">
+    <div className="container-fluid  bg-watchList pb-5">
       <div className="col-12 pt-4">
         <h4 className="text-center text-seventh fw-bold">
           WATCH LIST ({favoriteMovieData && favoriteMovieData.length})
         </h4>
       </div>
-      <div className="row row-cols-1 row-cols-md-6 g-4 p-4">
+      
+      <div className="row row-cols-1 row-cols-md-6 g-4 p-4 d-flex justify-content-center">
         {favoriteMovieData && favoriteMovieData.length > 0 ? (
           favoriteMovieData.map((item, index) => {
             return <MovieCard key={index} movieInfo={item} />;
           })
         ) : (
-          <div>
-            <p className="text-seventh">İzleme Listeniz Boş</p>
+          <div className="mt-5">
+            <p className="text-seventh text-center">Your Watch List is Empty</p>
           </div>
         )}
       </div>
+      
+
     </div>
   );
 }
